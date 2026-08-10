6b55c94248e10ec77f41ad267354a483e40718ac
박민규 <tig04162@gmail.com>
2026-08-10 18:19:33 +0900

    Vue.js 실습 1일차 커밋

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+  import SampleOne from './components/practices/basic/SampleOne.vue'
+  import SampleTwo from './components/practices/basic/SampleTwo.vue'
+  import VHtmlDirective from './components/practices/basic/V-Html-Directive.vue'
+  import VHtmlXSS from './components/practices/basic/V-Html-XSS.vue'
+  import VText from './components/practices/basic/V-Text.vue'
+  import VBind from './components/practices/basic/V-Bind.vue'
+  import VBindAdvanced from './components/practices/basic/V-Bind-Advanced.vue'
+  import VBindDynamic from './components/practices/basic/V-Bind-Dynamic.vue'
+  import VBindShort from './components/practices/basic/V-Bind-Short.vue'
+  import VIf from './components/practices/basic/V-If.vue'
+  import VShow from './components/practices/basic/V-Show.vue'
+  import VFor from './components/practices/basic/V-For.vue'
+</script>
+
+<template>
+  <main class="container page">
+    <h1 class="page-title">Vue.js Practice</h1>
+    <h2>1. Dev Setup</h2>
+    <SampleOne />
+    <SampleTwo />
+    <h2>2. Vue Directive</h2>
+    <VHtmlDirective />
+    <VHtmlXSS />
+    <VText />
+    <VBind />
+    <VBindAdvanced />
+    <VBindDynamic />
+    <VBindShort />
+    <VIf />
+    <VShow />
+    <VFor />
+  </main>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/practices/basic/V-Html-XSS.vue
@@ -0,0 +1,19 @@
+<script setup>
+    import { ref } from 'vue'
+
+    const inputValue = ref('')
+    const message = ref('')
+
+    function showMessage() {
+        message.value = inputValue.value
+    }
+</script>
+
+<template>
+    <div class="practice-section">
+        <h2>2.2 V-HMTL-XSS 학습</h2>
+        <input v-model="inputValue" placeholder="내용을 입력하세요" />
+        <button @click="showMessage">메시지 표시</button>
+        <p v-html="message"></p>
+    </div>
+</template>
